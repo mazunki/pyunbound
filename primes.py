4ae656f408a9c442a7db5127b8e7bf3362bc4390
Mazunki Hoksaas <rolferen@gmail.com>
2023-09-15 14:02:57 +0200

    first commit

---
+++ primes.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+from unbound import Unbound
+from typing import Optional
+import time
+
+class PrimeNumbers(Unbound):
+    def __init__(self, start=1, end: Optional[int]=None):
+        super().__init__(start=start, end=end)
+        self._primes = set()
+
+    def _create_generator(self):
+        num = self.start
+        while True:
+            if self.end is not None and num > self.end:
+                break
+            if num in self._primes:
+                break
+            for i in range(2, int(num ** 0.5) + 1):
+                if num % i == 0:
+                    break
+            else:
+                yield num
+            num += 1
+
+if __name__ == "__main__":
+    primes = PrimeNumbers(end=50)
+    derived = primes * 2
+    derived2 = derived - 1
+    print(primes, derived, derived2, sep="\n")
+
+    for p1, p2, p3 in zip(primes, derived, derived2):
+        print(p1, p2, p3)
+        time.sleep(0.1)
+
---
+++ unbound.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+from abc import ABC, abstractmethod
+from typing import Optional, Callable
+import copy
+
+class Unbound(ABC):
+    def __init__(self, start: int=1, end: Optional[int]=None):
+        self.start, self.end = start, end
+
+    @abstractmethod
+    def _create_generator(self):
+        num = self.start
+        while self.end is not None and num < self.end:
+            yield num
+            num += 1
+
+    def __iter__(self):
+        return self._create_generator()
+
+    def __add__(self, other):
+        return DerivedUnbound.derive(self, lambda v: v+other, expr=f"+{other}")
+
+    def __sub__(self, other):
+        return DerivedUnbound.derive(self, lambda v: v-other, expr=f"-{other}")
+
+    def __mul__(self, other):
+        return DerivedUnbound.derive(self, lambda v: v*other, expr=f"*{other}")
+
+    def __truediv__(self, other):
+        return DerivedUnbound.derive(self, lambda v: v/other, expr=f"/{other}")
+
+    def __floordiv__(self, other):
+        return DerivedUnbound.derive(self, lambda v: v//other, expr=f"//{other}")
+
+    def __pow__(self, other):
+        return DerivedUnbound.derive(self, lambda v: v**other, expr=f"**{other}")
+
+    def __str__(self):
+        return f"{self.__class__.__name__}({self.start}..{self.end or ''})"
+
+class DerivedUnbound(Unbound):
+    def __init__(self, base: Unbound, op: Callable, expr: str=""):
+        if base is None:
+            raise ValueError("base_class must be an iterable")
+
+        self.base = copy.copy(base)
+        self.operations: list[Callable] = [op]
+        self.expressions: list[str] = [expr]
+        super().__init__(start=base.start, end=base.end)
+
+    def __str__(self):
+        operation_str = ' -> '.join(self.expressions)
+        return f"<DerivedUnbound[{self.base}]: {operation_str}>"
+
+    def _create_generator(self):
+        generator = iter(self.base)
+        for operation in self.operations:
+            generator = map(operation, generator)
+        return generator
+
+    @classmethod
+    def derive(cls, base: Unbound, op: Callable, expr: str=""):
+        if isinstance(base, DerivedUnbound):
+            derived = copy.deepcopy(base)
+            derived.operations.append(op)
+            derived.expressions.append(expr)
+            return derived
+        else:
+            return cls(base=base, op=op, expr=expr)
